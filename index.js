58fa9e18fa384d345aa966862893e91bf8c0610f
Gezahegn Starr <starrgez@msu.edu>
2021-05-12 17:19:36 -0400

    server-work

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { db } = require("./db");
 const app = require("./api");
 const http = require("http");
-const PORT = 3002;
+const PORT = 3000;
 
 const httpServer = http.createServer(app);
 db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
