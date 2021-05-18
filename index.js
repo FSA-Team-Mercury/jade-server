f3c5b37e37367022231025c94971b08fb4a51578
Alan Alban <alan.alban10@gmail.com>
2021-05-18 14:30:15 -0400

    added PORT to server

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const { db } = require("./db");
+require("dotenv").config();
 const app = require("./api");
 const http = require("http");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const httpServer = http.createServer(app);
 db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
   .then(() => {
     console.log("db synced");
     httpServer.listen(PORT, () =>
-      console.log(`Express server listening at http://localhost:${PORT}`)
+      console.log(`Express server listening at PORT ${PORT}`)
     );
   });
