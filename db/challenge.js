4e7284b0569ee080c01573f2bf836489581a1964
Alan Alban <alan.alban10@gmail.com>
2021-05-21 13:50:23 -0400

    removed console logs

--- db/challenge.js
+++ db/challenge.js
@@ -5,7 +5,13 @@ const db = require("./database");
 
 const Challenge = db.define("challenge", {
   type: {
-    type: Sequelize.ENUM("big-saver", "big-spender", "hermit", "traveler", "smart-shopper"),
+    type: Sequelize.ENUM(
+      "big-saver",
+      "big-spender",
+      "hermit",
+      "traveler",
+      "smart-shopper"
+    ),
     allowNull: false,
   },
   startDate: {
@@ -25,9 +31,6 @@ const Challenge = db.define("challenge", {
 Challenge.beforeCreate((challenge) => {
   const end_date = moment(challenge.endDate).format("MM-DD-YYYY");
   challenge.endDate = String(end_date);
-
-  console.log('end-date--------->', end_date);
-  console.log('challenge', challenge)
 
   const start_date = moment(challenge.startDate).format("MM-DD-YYYY");
   challenge.startDate = String(start_date);
